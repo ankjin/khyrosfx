5ed991997761ebae2d91accc7a8e04d35c1a9222
Felix Matusinio <FM2150666@aabqatar.com>
2023-10-10 20:42:02 +0300

    change text from home page

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -29,22 +29,20 @@ export function HomePage() {
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
               <div className="mx-auto max-w-2xl text-center">
                 <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                  Deploy to the cloud with confidence One
+                TRUST & TRANSPARENCY
                 </h1>
                 <p className="mt-6 text-lg leading-8 text-gray-600">
-                  Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-                  lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-                  fugiat aliqua.
+                Our proven track record allows us to offer investors fixed returns via KFX BOND, while assuming all of the risk ourselves helping them invest in a safe way.
                 </p>
                 <div className="mt-10 flex items-center justify-center gap-x-6">
                   <a
-                    href="1"
+                    href="/"
                     className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                   >
                     Get started
                   </a>
                   <a
-                    href="2"
+                    href="/"
                     className="text-sm font-semibold leading-6 text-gray-900"
                   >
                     Learn more <span aria-hidden="true">→</span>
